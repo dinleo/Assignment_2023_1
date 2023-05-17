dfe997331cb3e015c6d0303f0c964e9de3e87928
Desktop <dinleo11@gmail.com>
2023-05-18 00:02:31 +0900

    hw4 complete

--- CP/hw4/code/optimizer.ml
+++ CP/hw4/code/optimizer.ml
@@ -7,8 +7,37 @@ match pgm with
 | [] -> []
 | inst :: _pgm -> begin
     match inst with
-    | (_, COPY(id, id2)) -> begin
-
+    | (l_o, FUNC_DEF(f,a_l,body)) -> (l_o, FUNC_DEF(f,a_l,(optimize_pgm body)))::(optimize_pgm _pgm)
+    | (l_o, COPY(id, id_o)) -> begin
+        let rec id2id_o t_pgm = begin
+        	match t_pgm with
+            | [] -> []
+            | inst_::_t_pgm -> begin
+                match inst_ with
+                | (l, RETURN(y)) when y=id -> (l, RETURN(id_o))::(id2id_o _t_pgm)
+                | (l, RANGE(x, y, z)) when y=id -> (l, RANGE(x, id_o, z))::(id2id_o _t_pgm)
+                | (l, RANGE(x, y, z)) when z=id -> (l, RANGE(x, y, id_o))::(id2id_o _t_pgm)
+                | (l, LIST_APPEND(a, y)) when y=id -> (l, LIST_APPEND(a, id_o))::(id2id_o _t_pgm)
+                | (l, LIST_INSERT(a, y)) when y=id -> (l, LIST_INSERT(a, id_o))::(id2id_o _t_pgm)
+                | (l, TUPLE_INSERT(a, y)) when y=id -> (l, TUPLE_INSERT(a, id_o))::(id2id_o _t_pgm)
+                | (l, ITER_LOAD(x, a, y)) when y=id -> (l, ITER_LOAD(x, a, id_o))::(id2id_o _t_pgm)
+                | (l, ITER_STORE(a, x, y)) when y=id -> (l, ITER_STORE(a, x, id_o))::(id2id_o _t_pgm)
+                | (l, ASSIGNV(x, bop, y, z)) when y=id -> (l, ASSIGNV(x, bop, id_o, z))::(id2id_o _t_pgm)
+                | (l, ASSIGNV(x, bop, y, z)) when z=id -> (l, ASSIGNV(x, bop, y, id_o))::(id2id_o _t_pgm)
+                | (l, ASSIGNC(x, bop, y, n)) when y=id -> (l, ASSIGNC(x, bop, id_o, n))::(id2id_o _t_pgm)
+                | (l, WRITE(y)) when y=id -> (l, WRITE(id_o))::(id2id_o _t_pgm)
+                | (l, READ(y)) when y=id -> (l, WRITE(id_o))::(id2id_o _t_pgm)
+                | _ -> inst_::(id2id_o _t_pgm)
+            end
+        end in
+        let new_pgm = (id2id_o _pgm) in
+        let check_pgm = (get_pgm_until_l target l_o)@new_pgm in
+        let usage = (find_id_usage check_pgm id) in
+        begin
+            match usage with
+            | [one_ins] when one_ins=inst -> optimize_pgm new_pgm      (*no usage for new_pgm -> can remove*)
+            | _ -> inst::(optimize_pgm _pgm)    (*have usage -> can't remove*)
+        end
     end
     | (_, COPYC(id, i)) -> begin
         let rec id2int t_pgm = begin
@@ -25,8 +54,8 @@ match pgm with
         let usage = (find_id_usage new_pgm id) in
         begin
             match usage with
-            | [] -> optimize_pgm new_pgm       (*no usage for new_pgm -> can remove*)
-            | _ -> inst::(optimize_pgm _pgm)   (*have usage -> can't remove*)
+            | [] -> optimize_pgm new_pgm
+            | _ -> inst::(optimize_pgm _pgm)
         end
     end
     | (_, COPYS(id, s)) -> begin
@@ -80,8 +109,8 @@ and find_id_usage t_pgm id = begin
 	| [] -> []
 	| inst_ :: _t_pgm -> begin
         match inst_ with
-        | (_, FUNC_DEF(_, id_list, _)) when (List.mem id id_list) -> inst_::(find_id_usage _t_pgm id)
-        | (_, CALL(x, _, id_list)) when ((List.mem id id_list)||(x=id)) -> inst_::(find_id_usage _t_pgm id)
+        | (_, FUNC_DEF(f, id_list, _)) when ((List.mem id id_list)||(f=id)) -> inst_::(find_id_usage _t_pgm id)
+        | (_, CALL(x, f, id_list)) when ((List.mem id id_list)||(f=id)||(x=id)) -> inst_::(find_id_usage _t_pgm id)
         | (_, RETURN(x)) when x=id -> inst_::(find_id_usage _t_pgm id)
         | (_, RANGE(x, lo, hi)) when (x=id || lo=id || hi=id) -> inst_::(find_id_usage _t_pgm id)
         | (_, LIST_EMPTY(x)) when x=id -> inst_::(find_id_usage _t_pgm id)
@@ -109,6 +138,16 @@ and find_id_usage t_pgm id = begin
         | (_, ASSERT(x)) when x=id -> inst_::(find_id_usage _t_pgm id)
         | _ -> (find_id_usage _t_pgm id)
     end
+end
+and get_pgm_until_l t_pgm u_l = begin
+	let rec find_b_pgm w_pgm = begin
+		match w_pgm with
+		| [] -> []
+		| [(l, ins)] when l=u_l -> [(l, ins)]
+		| (l, ins)::_ when l=u_l -> [(l, ins)]
+		| t_inst::_w_pgm -> t_inst::(find_b_pgm _w_pgm)
+	end in
+    (find_b_pgm t_pgm)
 end
 in
 optimize_pgm target
